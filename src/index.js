0fcf7582862873957d71e86509c0e28b75044080
Felix <felixfong03@live.com>
2021-07-24 10:53:38 -0400

    added a timer and beffed up the ui

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// Maybe add a timer
+
 // The number of times the user clicked
 let numClicks = 0;
 
+const counter = document.getElementById("counter");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    counter.textContent = (parseInt(counter.textContent) + 1).toString();
+});

6ccfd1e7f44a85e28d3a32bf871699a2b2ebaee8
Felix <felixfong03@live.com>
2021-07-24 14:38:16 -0400

    added timer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-// Maybe add a timer
-
 // The number of times the user clicked
 let numClicks = 0;
 
+const timer = document.getElementById("timer");
 const counter = document.getElementById("counter");
 const button = document.querySelector("button");
 
+const startTimer = () => {
+	let timeLeft = 100;
+	let countDown = setInterval(() => {
+		--timeLeft;
+		timer.textContent = `${Math.floor(timeLeft / 10).toString()}.${
+			timeLeft % 10
+		}s`;
+
+		// Exit
+		if (timeLeft <= 0) {
+			// Disable the button
+			button.disabled = true;
+			button.classList.add("disabledButton");
+			button.textContent = "Finished!";
+			clearInterval(countDown);
+		}
+	}, 100);
+};
+
 button.addEventListener("click", () => {
-    counter.textContent = (parseInt(counter.textContent) + 1).toString();
+	// Only execute the first time the button is clicked
+	if (counter.textContent === "0") {
+		startTimer();
+	}
+
+	counter.textContent = (parseInt(counter.textContent) + 1).toString();
 });
